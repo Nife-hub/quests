fc2e0eb666a57cb80e96b348686bfb9e43e56aa0
eadebamb <adebambonifemi07@gmail.com>
2026-04-16 17:37:31 +0100

    commit

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -1,11 +1,56 @@
 package main
 
-func NbrConvertAlpha(n int) string {
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func Atoi(s string) int {
+	n := 0
+	for i := 0; i < len(s); i++ {
+		if s[i] < '0' || s[i] > '9' {
+			return -1
+		}
+		n = n*10 + int(s[i]-'0')
+	}
+	return n
+}
+
+func PrintAlpha(n int, upper bool) {
 	if n <= 0 || n > 26 {
-		return ""
+		z01.PrintRune(' ')
+		return
 	}
-	if !(n >= 'a' && n <= 'z') {
-		return ""
+
+	if upper {
+		z01.PrintRune(rune('A' + n - 1))
+	} else {
+		z01.PrintRune(rune('a' + n - 1))
 	}
-	return string(rune('a' + n - 1))
+}
+
+func main() {
+	if len(os.Args) == 1 {
+		return
+	}
+
+	upper := false
+	start := 1
+
+	if os.Args[1] == "--upper" {
+		upper = true
+		start = 2
+	}
+
+	if start >= len(os.Args) {
+		return
+	}
+
+	for i := start; i < len(os.Args); i++ {
+		n := Atoi(os.Args[i])
+		PrintAlpha(n, upper)
+	}
+
+	z01.PrintRune('\n')
 }
